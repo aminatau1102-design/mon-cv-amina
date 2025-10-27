0ad30064ec71ceca806f647612c35d952715c31a
aminatau1102-design <aminatau1102@gmail.com>
2025-10-27 16:02:49 +0000

    second commit

---
+++ JS/Tp.js
@@ -0,0 +1,27 @@
+//alert("Bonjour la li2");
+//var x=10;
+//let y=3.5;
+//alert(x);
+
+//let age = prompt("veuillez donner votre age?");
+
+//let nombre = prompt("Donner un nombre");
+ //let i=1;
+//while(i <= 10){
+    //alert(nombre + "*" + i+ "=" + nombre* i );
+    //i++;
+//}
+function valider(){
+    let pswl1 = document.getElementById("psw").value;
+    var psw12= document.getElementById("pswe").value;
+    if( pswl1 ==pswl2){
+        alert("les deux mots de pass sont identiques");
+    }
+    else
+    alert("veuiller taper votre mot de pass");
+}
+
+
+
+
+
